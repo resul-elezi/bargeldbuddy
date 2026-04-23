1a49e7fc5c638928e668abeb2c8e520e59a0e444
Resul Elezi <resul_elezi@protonmail.com>
2026-04-23 11:57:42 +0200

    fix: tailwindcss 4 config issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
